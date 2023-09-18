fb0bd56cb6108317ad0031b405cdd6cdbc3b3a35
Mamad Khatibi <mkhb1375@gmail.com>
2023-09-18 23:13:43 +0330

    fix

--- src/Top_Analytics/TopAnalyticsCard.jsx
+++ src/Top_Analytics/TopAnalyticsCard.jsx
@@ -4,7 +4,7 @@ export default function TopAnalyticsCard({url,number,detail}) {
 
     return (
         <div className="flex mr-[1.5rem] bg-[white] p-2 lg:p-[1.5rem] cursor-[pointer]">
-            <img className="w-[2rem] h-[2rem] lg:w-[3rem] mr-[0.75rem]" src={url} alt={detail} />
+            <img className="w-[2rem] h-[2rem] lg:h-[auto] lg:w-[3rem] mr-[0.75rem]" src={url} alt={detail} />
             <div>
             <p className="text:-[0.9rem] lg:text-[1.1rem] font-[800]">{number+'+'}</p>
             <p className="text-[0.7rem] font-[400] lg:mr-[2.25rem]">{detail}</p>
